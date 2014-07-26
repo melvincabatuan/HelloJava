1c5908563698a78747222cd37ad53a6b6ed10e7a
melvincabatuan <melvincabatuan@gmail.com>
2014-07-26 21:42:14 +0800

    Read/Write Files

---
+++ IOFileDemo/src/LineNumberer.java
@@ -0,0 +1,40 @@
+//package com.cabatuan.cpeprog;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+/**
+ * This program applies line numbers to a file.
+ */
+public class LineNumberer {
+	public static void main(String[] args) throws FileNotFoundException {
+		// Prompt for the input and output file names
+
+		Scanner console = new Scanner(System.in);
+		System.out.print("Input file: "); /// Ex. home/cobalt/workspace/IOFileDemo/src/test.txt
+		String inputFileName = console.next();
+		System.out.print("Output file: "); /// Ex. /home/cobalt/workspace/IOFileDemo/bin/out.txt
+		String outputFileName = console.next();
+
+		// Construct the Scanner and PrintWriter objects for reading and writing
+
+		File inputFile = new File(inputFileName);
+		Scanner in = new Scanner(inputFile);
+		PrintWriter out = new PrintWriter(outputFileName);
+		int lineNumber = 1;
+
+		// Read the input and write the output
+
+		while (in.hasNextLine()) {
+			String line = in.nextLine();
+			out.println("/* " + lineNumber + " */ " + line);
+			lineNumber++;
+		}
+
+		in.close();
+		out.close();
+		console.close();
+	}
+}
